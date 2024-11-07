87b8f4140e677b098e9eeab8af237806c18694d4
abduraxim <tursunboyevabdurahim2004@gmial.com>
2024-11-07 19:14:22 +0500

    07.11.2024

---
+++ src/main/java/org/example/individualwork/IndividualworkApplication.java
@@ -0,0 +1,13 @@
+package org.example.individualwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/individualwork/IndividualworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.individualwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
